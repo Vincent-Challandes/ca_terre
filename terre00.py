c707f5c4eaa2bbeb10ce705d05e9e9b784b6a90e
Vincent Challandes <v.challandes@gmail.com>
2023-01-31 09:54:19 +0100

    terre00.py fini

---
+++ terre00.py
@@ -0,0 +1,16 @@
+alphabet = ["A", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+#alphabet_01 = "AbcdefghijKlmnopqrstuvwxyz"
+
+
+
+for i in alphabet:
+    i = i.lower()
+    print(i, end="")
+print()
+
+#for i in alphabet_01:
+    #i = i.lower()
+    #print(i, end="")
+#print()
+        
+
