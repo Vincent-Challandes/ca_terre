82a9dd7b772e834255a80aa3fe1e2845373ce7d1
Vincent Challandes <v.challandes@gmail.com>
2023-02-08 10:46:31 +0100

    terre00.py corrected

--- terre00.py
+++ terre00.py
@@ -1,16 +1,14 @@
-alphabet = ["A", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-#alphabet_01 = "AbcdefghijKlmnopqrstuvwxyz"
+# Function
+def print_alphabet():
+    for i in alphabet:
+        i = i.lower()
+        print(i, end="")
+    print()
 
+# Variables
+#alphabet = ["A", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+alphabet = "AbcdefghijKlmnopqrstuvwxyz"
 
-
-for i in alphabet:
-    i = i.lower()
-    print(i, end="")
-print()
-
-#for i in alphabet_01:
-    #i = i.lower()
-    #print(i, end="")
-#print()
-        
+# Resolution
+print_alphabet()
 
