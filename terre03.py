e452e1f71e8b6d6e2818d9ef088339f9f6578d97
Vincent Challandes <v.challandes@gmail.com>
2023-02-08 11:46:14 +0100

    terre03.py corrected

--- terre03.py
+++ terre03.py
@@ -1,26 +1,32 @@
+## Function and variables globals
 import sys
-arguments = sys.argv # on récupère les arguments donner par l'utilisateur
 
-alphabet = "AbcdefghijKlmnopqrstuVwxyz".lower() # on créer la liste (chaine de caractère mais percu comme liste pour boucle)qu'on utilise pour boucler
-#alphabet = alphabet.lower() # minuscule
+def affiche_alphabet_depuis():
+    for i in alphabet[indice_départ:]: # on boucle dans notre liste alphabet depuis l'indice de départ jusqu'a la fin
+        print(i, end = "") # on print sur la même ligne
+    print() # retour a la ligne
+ 
+alphabet = "AbcdefghijKlmnopqrstuVwxyz".lower() 
+
+
+## Error handling
 
 # on cherche à savoir si un argument à été donné
 # pour ça on va compté le nombre d'argument si nombre argument > 1 c'est qu'un argument à été donné
-nombre_arguments = len(arguments)
+if len(sys.argv) != 2:
+    print("erreur : Pas le bon nombre d'argument transmis au progamme.")
+    sys.exit()
 
-if nombre_arguments == 2:
-
-    lettre_depart = arguments[1].lower() # on isole la lettre de départ choisi par l'utilisateur
-    #lettre_depart = lettre_depart.lower() # minuscule comment ça même si l'utilisateur entre une majuscule ça fonctionne
-
-    if lettre_depart in alphabet: # on contrôle que l'arguement fasse parti de notre liste alphabet
-        indice_départ = alphabet.index(lettre_depart) # on recherche la position de cettre lettre dans notre liste alphabet
+# on contrôle que l'arguement fasse parti de notre liste alphabet
+if not sys.argv[1].lower() in alphabet: 
+    print("erreur : Ce n'est pas une lettre de l'alphabet.")
+    sys.exit()
 
 
-        for i in alphabet[indice_départ:]: # on boucle dans notre liste alphabet depuis l'indice de départ jusqu'a la fin
-            print(i, end = "") # on print sur la même ligne
-        print() # retour a la ligne
-    else:
-        print("Ce n'est pas une lettre de l'alphabet.")
-else:
-    print("Pas le bon nombre d'argument transmis au progamme.")
+## Parsing
+lettre_depart = sys.argv[1].lower()
+indice_départ = alphabet.index(lettre_depart) # on recherche la position de cettre lettre dans notre liste alphabet
+
+
+## Resolution
+affiche_alphabet_depuis()
