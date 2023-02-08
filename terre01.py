2bdaf5194dbee617a1224f0604ea2f09d67d3570
Vincent Challandes <v.challandes@gmail.com>
2023-02-08 14:55:28 +0100

    terre01.py corrected

--- terre01.py
+++ terre01.py
@@ -3,25 +3,30 @@ def name_file_v1():
     #solution sans le module os 
     chemin_liste = chemin.split("/") # joue pour mac pour windows il faudrait corriger le "/"
     nom = chemin_liste[-1]
-    print(nom)
+    return nom
 
 def name_file_v2():
     # solution avec module os
     import os
-    nom = os.path.basename(__file__)
-    print(nom)
+    nom_1 = os.path.basename(__file__)
+    return nom_1
 
 def name_file_v3():
     # solution avec module sys
     import sys
     nom = sys.argv
     for i in nom:
-        print(i)
+        return i
 
 # Variables
 chemin = __file__
 
 # Resolution
-name_file_v1()
-name_file_v2()
-name_file_v3()
+name_v1 = name_file_v1()
+name_v2 = name_file_v2()
+name_v3 = name_file_v3()
+
+# Display
+print(name_v1)
+print(name_v2)
+print(name_v3)
