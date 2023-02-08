5a57084d797e5aa21234972462c9452576d9be4b
Vincent Challandes <v.challandes@gmail.com>
2023-02-08 11:04:48 +0100

    terre01.py corrected

--- terre01.py
+++ terre01.py
@@ -1,17 +1,27 @@
+# Function
+def name_file_v1():
+    #solution sans le module os 
+    chemin_liste = chemin.split("/") # joue pour mac pour windows il faudrait corriger le "/"
+    nom = chemin_liste[-1]
+    print(nom)
+
+def name_file_v2():
+    # solution avec module os
+    import os
+    nom = os.path.basename(__file__)
+    print(nom)
+
+def name_file_v3():
+    # solution avec module sys
+    import sys
+    nom = sys.argv
+    for i in nom:
+        print(i)
+
+# Variables
 chemin = __file__
 
-#solution sans le module os 
-chemin_liste = chemin.split("/") # joue pour mac pour windows il faudrait corriger le "/"
-nom = chemin_liste[-1]
-print(nom)
-
-# solution avec module os
-import os
-nom_01 = os.path.basename(__file__)
-print(nom_01)
-
-# solution avec module sys
-import sys
-nom = sys.argv
-for i in nom:
-    print(i)
+# Resolution
+name_file_v1()
+name_file_v2()
+name_file_v3()
