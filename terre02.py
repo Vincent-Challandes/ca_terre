10fa5cffc33a748f530a5c190669aae2d765b995
Vincent Challandes <v.challandes@gmail.com>
2023-02-08 11:10:25 +0100

    terre02.py corrected

--- terre02.py
+++ terre02.py
@@ -1,7 +1,12 @@
-import sys
-arguments = sys.argv
-for i in arguments:
-    if i == arguments[0]:
-        continue
-    else:
-        print(i)
+# Function 
+def afficheur_arguments():
+    import sys
+    arguments = sys.argv
+    for i in arguments:
+        if i == arguments[0]:
+            continue
+        else:
+            print(i)
+
+# Resolution
+afficheur_arguments()
