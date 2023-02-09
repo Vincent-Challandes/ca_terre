8321fe9fa25be01e7ec787cd1a1723c2cafff585
Vincent Challandes <v.challandes@gmail.com>
2023-02-09 17:01:19 +0100

    terre09.py corrected

--- terre09.py
+++ terre09.py
@@ -7,6 +7,30 @@ def racine_carre(a):
      return result
 
 
+def racine_carre_manuelle(a):
+    # initialiser les valeurs minimales et maximales pour la bissection
+    min_val = 0
+    max_val = a
+
+    # définir une précision pour la méthode de bissection
+    precision = 0.0001
+
+    # utiliser la méthode de bissection pour trouver la racine carrée
+    while (max_val - min_val) > precision:
+        # trouver la valeur moyenne entre les valeurs minimale et maximale
+        moyenne = (min_val + max_val) / 2
+        carre = moyenne * moyenne
+
+        # déterminer si la valeur moyenne est plus grande ou plus petite que a
+        if carre < a:
+            min_val = moyenne
+        else:
+            max_val = moyenne
+    
+    # retourner la valeur moyenne finale comme la racine carrée
+    return moyenne
+
+
 ## Error handling
 
 # chek nb arguements
@@ -29,9 +53,17 @@ nombre = int(sys.argv[1])
 
 resultat = racine_carre(nombre)
 
+resultat_manuel = racine_carre_manuelle(nombre)
+
+resultat_arrondit = round(resultat)
+
+resultat_manuel_arrondit = round(resultat_manuel)
+
 
 ## Display
 
-print(resultat) # résultat du float donc nombre décimal
-resultat = int(resultat) # on ramene le float à un integer
-print(resultat) # résultat du integer donc nombre entier
+print(resultat) 
+print(resultat_arrondit)
+
+print(resultat_manuel) 
+print(resultat_manuel_arrondit)
