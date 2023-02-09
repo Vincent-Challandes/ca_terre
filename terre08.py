04d2db5b20e01b77646c302fa5c5f3c34b2771d3
Vincent Challandes <v.challandes@gmail.com>
2023-02-09 10:05:35 +0100

    terre08.py corrected

--- terre08.py
+++ terre08.py
@@ -1,21 +1,37 @@
+## Modules, Functions and Variables globals
+
 import sys
 
-# on récupère les arguments
-arguments = sys.argv
+def puissance_dun_nombre(a, b):
+     isolé_a = a
+     for i in range(b - 1):
+          a = a * isolé_a
+     return a
 
-# on check le nombre d'argument
-nombre_arguments = len(arguments)
+
+## Error handling
 
 # si on nous a transmis 2 arguements (nos deux nombres pour notre calcul de puissance)
-if nombre_arguments == 3:
-    nombre_1 = arguments[1]
-    nombre_2 = arguments[2]
-    # on contrôle que ce soit des nombre et que l'exposant soit positif
-    if nombre_1.lstrip("-").isdigit() and nombre_2.isdigit():
-         resultat = int(nombre_1) ** int(nombre_2)
-         print(resultat)
-    else:
-         print("erreur les arguement doivent être des nombre et l'exposant doit être positif")
-    
-else:
-     print("erreur pas le bon nombre d'arguments")
+if len(sys.argv) != 3:
+     print("erreur : Pas le bon nombre d'arguments veuillez entrer 2 nombres entiers")
+     sys.exit()
+
+# on contrôle que ce soit des nombre et que l'exposant soit positif
+if not sys.argv[1].lstrip("-").isdigit() or not sys.argv[2].isdigit():
+     print("erreur : Les arguement doivent être des nombre et l'exposant doit être positif")
+     sys.exit()
+
+
+## Parsing
+
+nombre, exposant = int(sys.argv[1]), int(sys.argv[2])
+
+
+## Resolution
+
+resultat = puissance_dun_nombre(nombre, exposant)
+
+
+## Display
+
+print(resultat)
